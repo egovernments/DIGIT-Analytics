45dfdab1a5e97161d109f35fedfa7df21089ccf8
nitish-egov <137176807+nitish-egov@users.noreply.github.com>
2025-08-29 14:46:17 +0530

    updated inbox object mapper to ignore uknown properties of user

--- inbox/src/main/java/org/egov/inbox/web/model/V2/SearchRequest.java
+++ inbox/src/main/java/org/egov/inbox/web/model/V2/SearchRequest.java
@@ -2,9 +2,11 @@ package org.egov.inbox.web.model.V2;
 
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 import org.egov.common.contract.request.RequestInfo;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
